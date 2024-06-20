4e86e8f4749ed83ed9e3782624c5f8edbecf0fc1
Harman Goei <5001432+hrgui@users.noreply.github.com>
2024-06-19 20:12:35 -0700

    refactor: remove unneeded code

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,5 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import SpatialNavigation from 'vue-spatial-nav/lib/spatial_navigation'
 
 const router = createRouter({
   history: createWebHashHistory(import.meta.env.BASE_URL),
