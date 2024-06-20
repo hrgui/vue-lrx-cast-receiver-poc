1b7d8d9aabbac04e149204b341ecfed6397a41df
Harman Goei <5001432+hrgui@users.noreply.github.com>
2024-06-19 20:27:49 -0700

    feat: navigation is vertical

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,17 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <nav v-focus-section:menu>
-        <RouterLink to="/" v-focus>Home</RouterLink>
-        <RouterLink to="/browse" v-focus>Browse</RouterLink>
-        <RouterLink to="/history" v-focus>History</RouterLink>
-        <RouterLink to="/search" v-focus>Search</RouterLink>
-        <RouterLink to="/watchlist" v-focus>Watchlist</RouterLink>
-        <RouterLink to="/settings" v-focus>Settings</RouterLink>
-      </nav>
+  <div style="display: flex">
+    <nav style="display: flex; flex-direction: column" v-focus-section:menu>
+      <RouterLink to="/" v-focus>Home</RouterLink>
+      <RouterLink to="/browse" v-focus>Browse</RouterLink>
+      <RouterLink to="/history" v-focus>History</RouterLink>
+      <RouterLink to="/search" v-focus>Search</RouterLink>
+      <RouterLink to="/watchlist" v-focus>Watchlist</RouterLink>
+      <RouterLink to="/settings" v-focus>Settings</RouterLink>
+    </nav>
+    <div>
+      <RouterView />
     </div>
-  </header>
-
-  <RouterView />
+  </div>
 </template>
